1bea0d8fecd6ce123d6b1dd199e189a36f33c5bb
Joe Pogson <joepog@hotmail.com>
2020-03-28 14:30:05 -0400

    Concluding R Script for submission

--- run_analysis.R
+++ run_analysis.R
@@ -1,22 +1,22 @@
 library(dplyr)
 
-# read train data
+# reading the train data
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
 Sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
-# read test data
+# reading the test data
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
 Sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
-# read data description
+# read the variable names
 variable_names <- read.table("./UCI HAR Dataset/features.txt")
 
-# read activity labels
+# read the activity labels
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
-# 1. Merges the training and the test sets to create one data set.
+# 1. Merges the training and the test sets into one data set.
 X_total <- rbind(X_train, X_test)
 Y_total <- rbind(Y_train, Y_test)
 Sub_total <- rbind(Sub_train, Sub_test)
@@ -37,5 +37,5 @@ colnames(X_total) <- variable_names[selected_var[,1],2]
 # of each variable for each activity and each subject.
 colnames(Sub_total) <- "subject"
 total <- cbind(X_total, activitylabel, Sub_total)
-total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
+total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(list(mean = mean, median = median))
 write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
