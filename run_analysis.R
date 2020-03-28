bad5e69932596a85561c3fb159126f611a21300f
Joe pogson <62182000+joepog@users.noreply.github.com>
2020-03-28 15:04:46 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,40 +1,33 @@
 library(dplyr)
 
-# reading the train data
+# read train and test data
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
 Sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-
-# reading the test data
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
 Sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
-# read the variable names
+# read the variable names and activity labels
 variable_names <- read.table("./UCI HAR Dataset/features.txt")
-
-# read the activity labels
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
-# 1. Merges the training and the test sets into one data set.
+# Merge the training and the test sets.
 X_total <- rbind(X_train, X_test)
 Y_total <- rbind(Y_train, Y_test)
 Sub_total <- rbind(Sub_train, Sub_test)
 
-# 2. Extracting mean and standard deviation for each measurement.
+# Extract mean and standard deviation.
 selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
 X_total <- X_total[,selected_var[,1]]
 
-# 3. Better labels activities in the data set
+# Create better labels for activities and variables
 colnames(Y_total) <- "activity"
 Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(activity_labels[,2]))
 activitylabel <- Y_total[,-1]
-
-# 4. Better labels for descriptive variable names.
 colnames(X_total) <- variable_names[selected_var[,1],2]
 
-# 5. Creates a second independent tidy data set with the average
-# of each variable for each activity and each subject.
+# Create an independent tidy data set.
 colnames(Sub_total) <- "subject"
 total <- cbind(X_total, activitylabel, Sub_total)
 total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(list(mean = mean, median = median))
